086611ea2cb19110484c2784742429353c751eca
warp <martin.derouet0@gmail.com>
2018-01-23 02:03:44 +0100

    Bad path missing espace character

---
+++ JavaLint/src/utility/FileUtils.java
@@ -0,0 +1,40 @@
+package utility;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import structure.JavaFile;
+
+public class FileUtils {
+
+	private List<JavaFile> javaFilePath = new ArrayList<JavaFile>();
+
+	public List<JavaFile> findAllFiles(String path) {
+		walk(path);
+		return javaFilePath;
+	}
+
+	private void walk(String path) {
+
+		File root = new File(path);
+		File[] list = root.listFiles();
+
+		if (list == null)
+			return;
+
+		for (File f : list) {
+			if (f.isDirectory()) {
+				walk(f.getAbsolutePath());
+			} else {
+				if (f.getAbsoluteFile().toString().toUpperCase().contains(".JAVA")) {
+					String pathCurrentFile = f.getPath();
+					int lastIndexSlash = pathCurrentFile.lastIndexOf("\\") + 1;
+					javaFilePath.add(new JavaFile(f.getParentFile(),
+							f.getPath().substring(lastIndexSlash, pathCurrentFile.length())));
+				}
+			}
+		}
+	}
+
+}
