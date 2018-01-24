43c57de7aa7c6efff5955aa632366bdcaf1b46f2
warp <warp@192.168.24.120>
2018-01-24 18:39:35 +0100

    Renaming files

--- JavaLint/src/utility/FileTreeUtils.java
+++ JavaLint/src/utility/FileTreeUtils.java
@@ -6,20 +6,19 @@ import java.util.List;
 
 import structure.JavaFile;
 
-public class FileUtils {
+public class FileTreeUtils {
 
 	private List<JavaFile> javaFilePath = new ArrayList<JavaFile>();
-	
+
 	/* FILE CURRENTLY SCANNED */
 	public static JavaFile currentFileProcessing = null;
-	
+
 	public List<JavaFile> findAllFiles(String path) {
 		walk(path);
 		return javaFilePath;
 	}
 
 	private void walk(String path) {
-
 		File root = new File(path);
 		File[] list = root.listFiles();
 
@@ -32,9 +31,8 @@ public class FileUtils {
 			} else {
 				if (f.getAbsoluteFile().toString().toUpperCase().contains(".JAVA")) {
 					String pathCurrentFile = f.getPath();
-					int lastIndexSlash = pathCurrentFile.lastIndexOf("\\") + 1;
-					javaFilePath.add(new JavaFile(f.getParentFile(),
-							f.getPath().substring(lastIndexSlash, pathCurrentFile.length())));
+					javaFilePath
+							.add(new JavaFile(f.getAbsoluteFile(), OSUtils.extractFileNameFromPath(pathCurrentFile)));
 				}
 			}
 		}
---
+++ JavaLint/src/utility/OSUtils.java
@@ -0,0 +1,48 @@
+package utility;
+
+public class OSUtils {
+
+	private static String OS = System.getProperty("os.name").toLowerCase();
+
+	public static boolean isWindows() {
+		return (OS.indexOf("win") >= 0);
+	}
+
+	public static boolean isMac() {
+		return (OS.indexOf("mac") >= 0);
+	}
+
+	public static boolean isUnix() {
+		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
+	}
+
+	public static boolean isSolaris() {
+		return (OS.indexOf("sunos") >= 0);
+	}
+
+	public static String getOS() {
+		if (isWindows()) {
+			return "win";
+		} else if (isMac()) {
+			return "osx";
+		} else if (isUnix()) {
+			return "uni";
+		} else if (isSolaris()) {
+			return "sol";
+		} else {
+			return "err";
+		}
+	}
+
+	public static String extractFileNameFromPath(String path) {
+		Integer lastIndexSlash = null;
+		if (isMac()) {
+			lastIndexSlash = path.lastIndexOf("/");
+		}
+		if (isWindows()) {
+			lastIndexSlash = path.lastIndexOf("\\");
+		}
+		return (path.substring(lastIndexSlash + 1, path.length()));
+	}
+
+}
