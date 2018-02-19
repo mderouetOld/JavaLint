8548c513e20e829167c177a10c789517850aab86
SECK <mbaye-kebe.seck@atos.net>
2018-02-19 09:27:50 +0100

    MKS : test de la règle classNameFormat

--- JavaLint/src/unit/test/GestionRulesTest.java
+++ JavaLint/src/unit/test/GestionRulesTest.java
@@ -32,4 +32,24 @@ public class GestionRulesTest extends TestCase {
 		assertEquals("Constant rule first error", Rule.LINE_SIZE, errorList.get(0).getRuleError());
 		assertEquals("Constant rule second error", Rule.LINE_SIZE, errorList.get(1).getRuleError());
 	}
+	// Test de classNameFormat
+	public void classNameFormat() {
+		if (OSUtils.isWindows()) {
+			FileTools.currentFileProcessing = new JavaFile(
+					new File(System.getProperty("user.dir").toString() + "\\tests\\unit_test\\ClassNameFormat.java"),
+					"ClassNameFormat");
+		}
+		if (OSUtils.isMac()) {
+			FileTools.currentFileProcessing = new JavaFile(
+					new File(System.getProperty("user.dir").toString() + "/tests/unit_test/ClassNameFormat.java"),
+					"ClassNameFormat");
+		}
+		List<RuleError> errorList = gestionRulesImpl.classNameFormat(null);
+		assertEquals("Number of line first error", 61, errorList.get(0).getLine().intValue());
+		assertEquals("Number of column first error", 306, errorList.get(0).getColumn().intValue());
+		assertEquals("Number of line second error", 82, errorList.get(1).getLine().intValue());
+		assertEquals("Number of column first error", 315, errorList.get(1).getColumn().intValue());
+		assertEquals("Constant rule first error", Rule.CLASS_NAME_FORMAT, errorList.get(0).getRuleError());
+		assertEquals("Constant rule second error", Rule.CLASS_NAME_FORMAT, errorList.get(1).getRuleError());
+	}
 }
